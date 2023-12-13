44e61ff90893c128349968e69a92941009a99d2c
OzanEFETopcu <ozanefe.topcu@gmail.com>
2023-12-13 03:52:11 +0200

    web clients done

--- fleet/src/main/java/com/ozanthongtomi/pizzeria/OrderController.java
+++ fleet/src/main/java/com/ozanthongtomi/pizzeria/OrderController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:3001/")
 @RequestMapping("/dronepizza")
 public class OrderController {
 
